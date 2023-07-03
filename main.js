0c788d99d737eda38481d6aa110c52294030562f
DK <shamo1995@naver.com>
2023-07-03 23:49:30 +0900

    second

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ timeTag = document.querySelector('.time b');
 flipsTag = document.querySelector('.flips b');
 refreshBtn = document.querySelector('.details button');
 
-let maxTime = 20;
+let maxTime = 30;
 let timeLeft = maxTime;
 let flips = 0;
 let matchedCards = 0;
@@ -22,6 +22,87 @@ function initTimer() {
 function flipCard({ target: clickedCard }) {
   if (!isPlaying) {
     isPlaying = true;
-    timer = setInterval(initTimer, 1000)
+    timer = setInterval(initTimer, 1000);
+  }
+  if (clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
+    flips++;
+    flipsTag.innerText = flips;
+    clickedCard.classList.add('flip');
+    if (!cardOne) {
+      return (cardOne = clickedCard);
+    }
+    cardTwo = clickedCard;
+    disableDeck = true;
+    let cardOneIcon = cardOne.querySelector('.back-view i').classList.value;
+    cardTwoIcon = cardTwo.querySelector('.back-view i').classList.value;
+    matchCards(cardOneIcon, cardTwoIcon);
   }
 }
+
+function matchCards(icon1, icon2) {
+  if (icon1 === icon2) {
+    matchedCards++;
+    if (matchedCards == 6 && timeLeft > 0) {
+      return clearInterval(timer);
+    }
+    cardOne.removeEventListener('click', flipCard);
+    cardTwo.removeEventListener('click', flipCard);
+    cardOne = cardTwo = '';
+    return (disableDeck = false);
+  }
+
+  setTimeout(() => {
+    cardOne.classList.add('shake');
+    cardTwo.classList.add('shake');
+  }, 400);
+
+  setTimeout(() => {
+    cardOne.classList.remove('shake', 'flip');
+    cardTwo.classList.remove('shake', 'flip');
+    cardOne = cardTwo = '';
+    disableDeck = false;
+  }, 1200);
+}
+
+function shuffleCards() {
+  timeLeft = maxTime;
+  flips = matchedCards = 0;
+  cardOne = cardTwo = '';
+  clearInterval(timer);
+  timeTag.innerText = timeLeft;
+  flipsTag.innerText = flips;
+  disableDeck = isPlaying = false;
+
+  let arr = [
+    'bx bxl-discord-alt',
+    'bx bxl-twitter',
+    'bx bxl-apple',
+    'bx bxl-windows',
+    'bx bxl-youtube',
+    'bx bxl-firefox',
+    'bx bxl-discord-alt',
+    'bx bxl-twitter',
+    'bx bxl-apple',
+    'bx bxl-windows',
+    'bx bxl-youtube',
+    'bx bxl-firefox',
+  ];
+  arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
+
+  cards.forEach((card, index) => {
+    card.classList.remove('flip');
+    let iconTag = card.querySelector('.back-view i');
+    setTimeout(() => {
+      iconTag.classList.value = `bx ${arr[index]}`;
+    }, 500);
+    card.addEventListener('click', flipCard);
+  });
+}
+
+shuffleCards();
+
+refreshBtn.addEventListener('click',shuffleCards);
+
+cards.forEach(card => {
+  card.addEventListener('click', flipCard)
+})
